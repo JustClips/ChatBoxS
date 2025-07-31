06fe28b109e57981f42ed783f72b3184956f850e
JustClips <135924737+JustClips@users.noreply.github.com>
2025-07-31 14:09:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors    = require("cors");
-const fetch   = require("node-fetch");
+// REMOVED: const fetch   = require("node-fetch");
 
 const app      = express();
 const PORT     = process.env.PORT || 8080;
